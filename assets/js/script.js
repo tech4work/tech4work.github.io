ff266e4dd4bccafb333b045de99b8b3777bd023c
Sofiane Imadali <sofiane.imadali@orange.com>
2022-04-05 11:08:45 +0200

    new privacy and banner

--- assets/js/script.js
+++ assets/js/script.js
@@ -24,3 +24,11 @@
 	});
 
 })(jQuery);
+
+// cookie banner
+if (localStorage.getItem('cookieSeen') != 'shown') {
+  $('.cookie-banner').delay(2000).fadeIn();
+  localStorage.setItem('cookieSeen','shown')
+};$('.close').click(function() {
+  $('.cookie-banner').fadeOut();
+})
